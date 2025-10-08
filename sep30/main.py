831eac2a81aaf9ba6cc6330044904cb75b570c9c
narayanangr7 <narayanangr7@gmail.com>
2025-10-08 14:46:53 +0530

    added a new content

--- sep30/main.py
+++ sep30/main.py
@@ -6,3 +6,10 @@ else:
     
 #=====================================================================
 
+def greeting(username,place,id_no):
+    #start
+    print("username: ", username)
+    print("place: ",place)
+    print("ID: ",id_no)
+    #end
+greeting("John","Chennai",100)
