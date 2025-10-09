9eaab0f8a2d7d9dabcfdab87cb51be4b9b7ee5dc
narayanangr7 <narayanangr7@gmail.com>
2025-10-09 14:10:46 +0530

    add a new python code

---
+++ oct9/main.py
@@ -0,0 +1,26 @@
+def day_check(day):
+    if type(day)==str:
+        print("Invalid input")
+    else:
+        print("ok")
+
+        match day:
+            
+            case 1:
+                print("monday")
+            case 2:
+                print("Tuestday")
+            case 4:
+                print("wednesday")
+            case 5:
+                print("Thureday")
+            case 6:
+                print("friday")
+            case 7:
+                print("saterday")
+            case _:
+                print("Enter number between 1 and 7 only ")
+
+
+#=====================================================================
+
